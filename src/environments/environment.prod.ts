8af4d93b4e717f5404cf1bdb4c1ac8d8649a0ce7
Rafael Martinez Estevez <malayibiri@gmail.com>
2022-11-08 14:47:23 -0500

    deploly

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,15 +2,15 @@ import { Environment } from 'toco-lib';
 
 class EnvironmentImpl implements Environment {
   production = false;
-  sceibaHost = 'https://sceiba.cu';
-  cuorHost = 'https://organizaciones.sceiba.cu/';
-  sceibaApi = 'https://sceiba.cu/api/';
-  cuorApi = 'https://organizaciones.sceiba.cu/api/';
+  sceibaHost = 'https://cuba-iroko.sceiba.org/';
+  cuorHost = 'https://cuba-iroko.sceiba.org/';
+  sceibaApi = 'https://cuba-iroko.sceiba.org/api/';
+  cuorApi = 'https://cuba-iroko.sceiba.org/api/';
 
-  appHost = 'https://organizaciones.sceiba.cu';
+  appHost = 'https://cuba-organizaciones.sceiba.org';
   appName = 'Orgs';
-  oauthRedirectUri = 'https://organizaciones.sceiba.cu/';
-  oauthClientId = 'ICC1j7NOH0067SgsMyKUXM9ZipavAXHPrbW1ll3V';
+  oauthRedirectUri = 'https://cuba-organizaciones.sceiba.org/';
+  oauthClientId = 'UZT1hqcvT1G2sMLN21LPWiML5yLZbRw7WZk0uPjE';
   oauthScope = 'user:email';
   topOrganizationPID = '';
   cachableUrls = [];
@@ -21,6 +21,16 @@ class EnvironmentImpl implements Environment {
 
   matomoUrl = 'https://crai-stats.upr.edu.cu/';
   matomoSiteId = 7;
+
+  sceiba = 'https://cuba.sceiba.org';
+  discover = 'https://cuba.sceiba.org/search';
+  catalog = 'https://cuba-catalogo.sceiba.org/';
+  revistasmes = 'https://cuba-revistasmes.sceiba.org/';
+  organizations = 'https://cuba-organizaciones.sceiba.org/';
+  persons = 'https://cuba-personas.sceiba.org/';
+  vocabularies = 'https://vocabularios.sceiba.cu/';
+  moodle = 'https://courses.sceiba.org/';
+  evaluations = 'https://evaluaciones.sceiba.org/';
 }
 
 export const environment = new EnvironmentImpl();
