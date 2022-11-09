cd783aa6c26d402313f772ddb80e671a675cbfd2
Rafael Martinez Estevez <malayibiri@gmail.com>
2022-11-09 14:16:51 -0500

    compilation

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 ng build  organizations --prod
+
+ng build  --prod --aot --preserve-symlinks --build-optimizer=true  --verbose  --baseHref https://cuba-organizaciones.sceiba --deploy-url https://cuba-organizaciones.sceiba/
