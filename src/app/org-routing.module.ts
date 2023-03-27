98aa80f243f85b52ac3278b28707964d885bcf3a
acm-97 <alejandro97.acm@gmail.com>
2023-03-26 20:50:45 -0400

    @angular/core migration - migration-v15-relative-link-resolution
    In Angular version 15, the deprecated `relativeLinkResolution` config parameter of the Router is removed. This migration removes all `relativeLinkResolution` fields from the Router config objects.

--- src/app/org-routing.module.ts
+++ src/app/org-routing.module.ts
@@ -183,7 +183,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+	imports: [RouterModule.forRoot(routes, {})],
 	exports: [RouterModule],
 	providers: [UserService, CuratorPermissionService, AdminPermissionService]
 })
